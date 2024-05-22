7a68ad3ed6aa8e0020ae9ee887c589b29f0cf058
jurgelionis <ljurgelionis@gmail.com>
2024-05-22 13:38:16 +0300

    add: organization city config key

--- src/ExternalSearch.Providers.GoogleMaps/GoogleMapsExternalSearchJobData.cs
+++ src/ExternalSearch.Providers.GoogleMaps/GoogleMapsExternalSearchJobData.cs
@@ -12,6 +12,7 @@ namespace CluedIn.ExternalSearch.Providers.GoogleMaps
             OrgNameKey = GetValue<string>(configuration, Constants.KeyName.OrgNameKey);
             OrgAddressKey = GetValue<string>(configuration, Constants.KeyName.OrgAddressKey);
             OrgZipCodeKey = GetValue<string>(configuration, Constants.KeyName.OrgZipCodeKey);
+            OrgCityKey = GetValue<string>(configuration, Constants.KeyName.OrgCityKey);
             OrgStateKey = GetValue<string>(configuration, Constants.KeyName.OrgStateKey);
             OrgCountryKey = GetValue<string>(configuration, Constants.KeyName.OrgCountryKey);
             LocationAddressKey = GetValue<string>(configuration, Constants.KeyName.LocationAddressKey);
@@ -30,6 +31,7 @@ namespace CluedIn.ExternalSearch.Providers.GoogleMaps
                 { Constants.KeyName.OrgNameKey, OrgNameKey },
                 { Constants.KeyName.OrgAddressKey, OrgAddressKey },
                 { Constants.KeyName.OrgZipCodeKey, OrgZipCodeKey },
+                { Constants.KeyName.OrgCityKey, OrgCityKey },
                 { Constants.KeyName.OrgStateKey, OrgStateKey },
                 { Constants.KeyName.OrgCountryKey, OrgCountryKey },
                 { Constants.KeyName.LocationAddressKey, LocationAddressKey },
@@ -46,6 +48,7 @@ namespace CluedIn.ExternalSearch.Providers.GoogleMaps
         public string OrgNameKey { get; set; }
         public string OrgAddressKey { get; set; }
         public string OrgZipCodeKey { get; set; }
+        public string OrgCityKey { get; set; }
         public string OrgStateKey { get; set; }
         public string OrgCountryKey { get; set; }
         public string LocationAddressKey { get; set; }
