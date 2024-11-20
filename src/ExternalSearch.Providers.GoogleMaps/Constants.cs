dfc6ce63fe9031d1ea2b9a76c41678ed14fc74a4
Yi-Wei <ywei390@gmail.com>
2024-11-20 15:10:29 +0800

    feat: Add guidance note to enricher configuration page

--- src/ExternalSearch.Providers.GoogleMaps/Constants.cs
+++ src/ExternalSearch.Providers.GoogleMaps/Constants.cs
@@ -10,6 +10,39 @@ namespace CluedIn.ExternalSearch.Providers.GoogleMaps
         public const string ComponentName = "GoogleMaps";
         public const string ProviderName = "Google Maps";
         public static readonly Guid ProviderId = Guid.Parse("7999344b-2ee6-462a-886a-a630b169117c");
+        public const string Instruction = """
+            [
+              {
+                "type": "bulleted-list",
+                "children": [
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "(For some enrichers) Add the API key to enable the enricher to retrieve information from a specific API. For example, the Vatlayer enricher requires an access key to authenticate with the Vatlayer API."
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+            """;
 
         public struct KeyName
         {
@@ -157,7 +190,10 @@ namespace CluedIn.ExternalSearch.Providers.GoogleMaps
             //}
         };
 
-        public static Guide Guide { get; set; } = null;
+        public static Guide Guide { get; set; } = new Guide
+        {
+            Instructions = Instruction
+        };
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
     }
 }
