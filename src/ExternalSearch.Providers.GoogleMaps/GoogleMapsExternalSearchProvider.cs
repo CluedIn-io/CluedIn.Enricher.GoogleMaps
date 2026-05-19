2aaab35a09b647e35389b933589e0fda6e5334f4
Yi-Wei <ywei390@gmail.com>
2026-05-19 15:37:52 +0800

    fix: Update company metadata code generation

--- src/ExternalSearch.Providers.GoogleMaps/GoogleMapsExternalSearchProvider.cs
+++ src/ExternalSearch.Providers.GoogleMaps/GoogleMapsExternalSearchProvider.cs
@@ -654,7 +654,8 @@ namespace CluedIn.ExternalSearch.Providers.GoogleMaps
 
         private void PopulateCompanyMetadata(IEntityMetadata metadata, IExternalSearchQueryResult<CompanyDetailsResponse> resultItem, IExternalSearchRequest request)
         {
-            var code = new EntityCode(request.EntityMetaData.EntityType, "googlemaps", $"{request.Queries.FirstOrDefault()?.QueryKey}{request.EntityMetaData.OriginEntityCode}".ToDeterministicGuid());
+            var queryKey = request.Queries.FirstOrDefault(x => x.Id == resultItem.QueryId)?.QueryKey ?? request.Queries.FirstOrDefault()?.QueryKey;
+            var code = new EntityCode(request.EntityMetaData.EntityType, "googlemaps", $"{queryKey}{request.EntityMetaData.OriginEntityCode}".ToDeterministicGuid());
 
             metadata.EntityType = request.EntityMetaData.EntityType;
             metadata.Name = request.EntityMetaData.Name;
